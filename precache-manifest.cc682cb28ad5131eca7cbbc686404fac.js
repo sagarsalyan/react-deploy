41610cab9cc6e887ba060ef845c70e26519fd956
sagarsalyan <sagarslyn@gmail.com>
2020-05-31 15:54:11 +0530

    Updates

---
+++ precache-manifest.cc682cb28ad5131eca7cbbc686404fac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7973c1a6843b0b932384e1be1e6d1c88",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "b4816a8d079db314b44c",
+    "url": "/react-deploy/static/css/main.0af11778.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4816a8d079db314b44c",
+    "url": "/react-deploy/static/js/main.b897663c.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4de7af8597efd71409460727d2bb40c9",
+    "url": "/react-deploy/static/media/sagar.4de7af85.png"
+  }
+]);
